a0ea9c1c7c732ba1cbaa451704818d11c276cb21
Tural Novruzov <tural.nt.1@gmail.com>
2020-08-28 17:09:23 +0400

    Added title, playxbutton, playobutton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 # Initialize pygame
 pygame.init()
@@ -9,8 +10,18 @@ SIZE = WIDTH, HEIGHT = 900, 600
 pygame.display.set_caption('TicTacToe')
 
 # Set the screen
-WINDOW = pygame.display.set_mode(SIZE)
+screen = pygame.display.set_mode(SIZE)
 
+# Fonts
+MEDIUM_FONT = pygame.font.Font(os.path.join('fonts', 'OpenSans-Regular.ttf'), 40)
+LARGE_FONT = pygame.font.Font(os.path.join('fonts', 'OpenSans-Regular.ttf'), 60)
+
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# States
 MENU_STATE = 'Menu'
 PLAY_STATE = 'Play'
 
@@ -25,4 +36,31 @@ while True:
             pygame.quit()
             exit()
 
-    
+    if gamestate == MENU_STATE:
+
+        # Background color
+        screen.fill(BLACK)
+
+        # Title
+        title = LARGE_FONT.render('Play Tic-Tac-Toe', True, WHITE)
+        title_rect = title.get_rect()
+        title_rect.center = (WIDTH / 2, 100)
+        screen.blit(title, title_rect)
+        
+        # Play as X button
+        playX = MEDIUM_FONT.render('Play as X', True, BLACK)
+        playXRect = playX.get_rect()
+        playXButton = pygame.Rect(WIDTH / 2 - (playXRect.width + 70) / 2, HEIGHT / 2 - (playXRect.height + 30), playXRect.width + 70, playXRect.height + 20)
+        playXRect.center = playXButton.center
+        pygame.draw.rect(screen, WHITE, playXButton)
+        screen.blit(playX, playXRect)
+
+        # Play as O button
+        playO = MEDIUM_FONT.render('Play as O', True, BLACK)
+        playORect = playO.get_rect()
+        playOButton = pygame.Rect(WIDTH / 2 - (playORect.width + 70) / 2, HEIGHT / 2 + 30, playORect.width + 70, playORect.height + 20)
+        playORect.center = playOButton.center
+        pygame.draw.rect(screen, WHITE, playOButton)
+        screen.blit(playO, playORect)
+
+    pygame.display.flip()
