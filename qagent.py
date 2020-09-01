516be91f53fd2074f0d60bf74ff284c31b339372
Tural Novruzov <tural.nt.1@gmail.com>
2020-09-01 16:03:49 +0400

    Implemented QAgent.__init__

--- qagent.py
+++ qagent.py
@@ -2,3 +2,17 @@ class QAgent:
     """
     A q-learning agent for Tictactoe
     """
+
+    def __init__(self, alpha, epsilon):
+        """
+        Initializes the QAgent
+
+        Args:
+            alpha (float): Learning rate
+            epsilon (float): Probability of choosing a random action
+        """
+        self.alpha = alpha
+        self.epsilon = epsilon
+
+        # Q table
+        self.Q = dict()
