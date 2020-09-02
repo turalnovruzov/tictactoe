ccac0718eb2072abd799c8221ffe283885148aa6
Tural Novruzov <tural.nt.1@gmail.com>
2020-09-02 21:26:18 +0400

    Implemented args -l and -f for train_agent.py

--- train_agent.py
+++ train_agent.py
@@ -5,4 +5,12 @@ from qagent import QAgent
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--filepath', type=str, default='Q.pkl',
                     help='Full or relative path of a file in which the agent is stored')
+parser.add_argument('-l', '--load', action='store_true', help='Load agent from file')
 args = parser.parse_args()
+
+agent = QAgent
+
+if args.load:
+    agent.load(args.filepath)
+
+agent.save(args.filepath)
